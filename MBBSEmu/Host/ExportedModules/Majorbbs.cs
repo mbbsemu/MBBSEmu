de36942ff3c1415d44cbd68fb3afc67b331a1852
enusbaum <eric@enusbaum.com>
2019-12-02 08:54:37 -0500

    More Functions

--- MBBSEmu/Host/ExportedModules/Majorbbs.cs
+++ MBBSEmu/Host/ExportedModules/Majorbbs.cs
@@ -1,12 +1,12 @@
 ﻿using MBBSEmu.CPU;
 using MBBSEmu.Logging;
+using MBBSEmu.Module;
 using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using MBBSEmu.Module;
 
 namespace MBBSEmu.Host
 {
@@ -25,8 +25,8 @@ namespace MBBSEmu.Host
         private readonly CpuCore _cpu;
         private readonly MbbsModule _module;
 
-        private readonly Dictionary<int, McvFile> McvFiles;
-        private McvFile CurrentMcvFile;
+        private readonly Dictionary<int, McvFile> _mcvFiles;
+        private McvFile _currentMcvFile;
 
         /// <summary>
         ///     Imported Functions from the REGISTER_MODULE method are saved here.
@@ -42,7 +42,7 @@ namespace MBBSEmu.Host
             _cpu = cpuCore;
             _module = module;
             ModuleRoutines = new Dictionary<string, Tuple<int, int>>();
-            McvFiles = new Dictionary<int, McvFile>();
+            _mcvFiles = new Dictionary<int, McvFile>();
         }
 
         /// <summary>
@@ -161,22 +161,10 @@ namespace MBBSEmu.Host
             var srcSegment = _cpu.Memory.Pop(_cpu.Registers.BP + 10);
 
             var inputBuffer = new MemoryStream();
-            int bytesCopied = 0;
-            if (srcSegment == 0xFFFF)
-            {
-                for (var i = 0; i < ushort.MaxValue; i++)
-                {
-                    bytesCopied++;
-                    byte inputByte = (byte)_mbbsHostMemory.GetHostByte(srcOffset + i);
-                    inputBuffer.WriteByte(inputByte);
-                    if (inputByte == 0)
-                        break;
-                }
-            }
-            else
-            {
-                inputBuffer.Write(_cpu.Memory.GetString(srcSegment, srcOffset));
-            }
+
+            inputBuffer.Write(srcSegment == 0xFFFF
+                ? _mbbsHostMemory.GetString(0, srcOffset)
+                : _cpu.Memory.GetString(srcSegment, srcOffset));
 
             if (destinationSegment == 0xFFFF)
             {
@@ -188,7 +176,7 @@ namespace MBBSEmu.Host
             }
 
 #if DEBUG
-            _logger.Info($"stzcpy() copied {bytesCopied} bytes from {srcSegment:X4}:{srcOffset:X4} to {destinationSegment:X4}:{destinationOffset:X4}");
+            _logger.Info($"stzcpy() copied {inputBuffer.Length} bytes from {srcSegment:X4}:{srcOffset:X4} to {destinationSegment:X4}:{destinationOffset:X4}");
 #endif
 
             _cpu.Registers.AX = destinationOffset;
@@ -321,17 +309,17 @@ namespace MBBSEmu.Host
 
             msgFileName = msgFileName.TrimEnd('\0');
 
-            CurrentMcvFile = new McvFile(msgFileName, _module.ModulePath);
+            _currentMcvFile = new McvFile(msgFileName, _module.ModulePath);
 
-            if (McvFiles.Count == 0 || McvFiles.Values.All(x => x.FileName != msgFileName))
-                McvFiles.Add(McvFiles.Count, CurrentMcvFile);
+            if (_mcvFiles.Count == 0 || _mcvFiles.Values.All(x => x.FileName != msgFileName))
+                _mcvFiles.Add(_mcvFiles.Count, _currentMcvFile);
 
 #if DEBUG
             _logger.Info(
                 $"opnmsg() opened MSG file: {msgFileName}, assigned to {(int) EnumHostSegments.MsgPointer:X4}:1");
 #endif
 
-            _cpu.Registers.AX = McvFiles.Count - 1;
+            _cpu.Registers.AX = _mcvFiles.Count - 1;
             _cpu.Registers.DX = (int) EnumHostSegments.MsgPointer;
         }
 
@@ -344,14 +332,14 @@ namespace MBBSEmu.Host
         [ExportedModuleFunction(Name = "NUMOPT", Ordinal = 441)]
         public void numopt()
         {
-            if(McvFiles.Count == 0)
+            if(_mcvFiles.Count == 0)
                 throw new Exception("Attempted to read configuration value from MSG file prior to calling opnmsg()");
 
             var msgnum = _cpu.Memory.Pop(_cpu.Registers.BP + 4);
             var floor = _cpu.Memory.Pop(_cpu.Registers.BP + 6);
             var ceiling = _cpu.Memory.Pop(_cpu.Registers.BP + 8);
 
-            var outputValue = CurrentMcvFile.GetNumeric(msgnum);
+            var outputValue = _currentMcvFile.GetNumeric(msgnum);
 
             //Validate
             if(outputValue < floor || outputValue >  ceiling)
@@ -374,7 +362,7 @@ namespace MBBSEmu.Host
         {
             var msgnum = _cpu.Memory.Pop(_cpu.Registers.BP + 4);
 
-            var outputValue = CurrentMcvFile.GetBool(msgnum);
+            var outputValue = _currentMcvFile.GetBool(msgnum);
 
 #if DEBUG
             _logger.Info($"ynopt() retrieved option {msgnum} value: {outputValue}");
@@ -404,7 +392,7 @@ namespace MBBSEmu.Host
             var floor = floorHigh << 16 | floorLow;
             var ceiling = ceilingHigh << 16 | ceilingLow;
 
-            var outputValue = CurrentMcvFile.GetLong(msgnum);
+            var outputValue = _currentMcvFile.GetLong(msgnum);
 
             //Validate
             if (outputValue < floor || outputValue > ceiling)
@@ -430,7 +418,7 @@ namespace MBBSEmu.Host
         {
             var msgnum = _cpu.Memory.Pop(_cpu.Registers.BP + 4);
 
-            var outputValue = CurrentMcvFile.GetString(msgnum);
+            var outputValue = _currentMcvFile.GetString(msgnum);
 
             var outputValueOffset = _mbbsHostMemory.AllocateHostMemory(outputValue.Length);
             _mbbsHostMemory.SetHostArray(outputValueOffset, Encoding.ASCII.GetBytes(outputValue));
@@ -501,32 +489,85 @@ namespace MBBSEmu.Host
             var sourceSegment = _cpu.Memory.Pop(_cpu.Registers.BP + 6);
 
             var inputBuffer = new MemoryStream();
-            int bytesCopied = 0;
-            if (sourceSegment == 0xFFFF)
-            {
-                for (var i = 0; i < ushort.MaxValue; i++)
-                {
-                    bytesCopied++;
-                    byte inputByte = (byte)_mbbsHostMemory.GetHostByte(sourceOffset + i);
-                    inputBuffer.WriteByte(inputByte);
-                    if (inputByte == 0)
-                        break;
-                }
-            }
-            else
-            {
-                inputBuffer.Write(_cpu.Memory.GetString(sourceSegment, sourceOffset));
-            }
+
+            inputBuffer.Write(sourceSegment == 0xFFFF
+                ? _mbbsHostMemory.GetString(0, sourceOffset)
+                : _cpu.Memory.GetString(sourceSegment, sourceOffset));
 
             var inputValue = Encoding.ASCII.GetString(inputBuffer.ToArray());
 
             if (!int.TryParse(inputValue, out var outputValue))
                 throw new InvalidCastException($"atol(): Unable to cast string value located at {sourceSegment:X4}:{sourceOffset:X4} to long");
 
+#if DEBUG
+            _logger.Info($"atol() cast {inputBuffer} ({sourceSegment:X4}:{sourceOffset:X4}) to long");
+#endif
+
             _cpu.Registers.AX = (int)(outputValue & 0xFFFF0000);
             _cpu.Registers.DX = outputValue & 0xFFFF;
+        }
 
-            
+        /// <summary>
+        ///     Find out today's date coded as YYYYYYYMMMMDDDDD
+        ///
+        ///     Signature: int date=today()
+        ///     Return: AX = Packed Date
+        /// </summary>
+        [ExportedModuleFunction(Name = "TODAY", Ordinal = 601)]
+        public void today()
+        {
+            //From DOSFACE.H:
+            //#define dddate(mon,day,year) (((mon)<<5)+(day)+(((year)-1980)<<9))
+
+            var packedDate = (DateTime.Now.Month << 5) + DateTime.Now.Day + (DateTime.Now.Year << 9);
+
+#if DEBUG
+            _logger.Info($"today() returned packed date: {packedDate}");
+#endif
+            _cpu.Registers.AX = (ushort)packedDate;
+        }
+
+        /// <summary>
+        ///     Copies Struct into another Struct (Borland C++ Implicit Function)
+        /// </summary>
+        public void f_scopy()
+        {
+
+        }
+
+        /// <summary>
+        ///     Case ignoring string match
+        ///
+        ///     Signature: int match=sameas(char *stgl, char* stg2)
+        ///     Returns: AX = 1 if match
+        /// </summary>
+        [ExportedModuleFunction(Name = "SAMEAS", Ordinal = 520)]
+        public void sameas()
+        {
+            var string1Offset = _cpu.Memory.Pop(_cpu.Registers.BP + 4);
+            var string1Segment = _cpu.Memory.Pop(_cpu.Registers.BP + 6);
+            var string2Offset = _cpu.Memory.Pop(_cpu.Registers.BP + 8);
+            var string2Segment = _cpu.Memory.Pop(_cpu.Registers.BP + 10);
+
+            var string1InputBuffer = new MemoryStream();
+            string1InputBuffer.Write(string1Segment == 0xFFFF
+                ? _mbbsHostMemory.GetString(0, string1Offset)
+                : _cpu.Memory.GetString(string1Segment, string1Offset));
+
+            var string1InputValue = Encoding.ASCII.GetString(string1InputBuffer.ToArray()).ToUpper();
+
+            var string2InputBuffer = new MemoryStream();
+            string1InputBuffer.Write(string2Segment == 0xFFFF
+                ? _mbbsHostMemory.GetString(0, string2Offset)
+                : _cpu.Memory.GetString(string2Segment, string2Offset));
+
+            var string2InputValue = Encoding.ASCII.GetString(string2InputBuffer.ToArray()).ToUpper();
+
+            var result = string1InputValue == string2InputValue;
+#if DEBUG
+            _logger.Info($"sameas() returned {result} comparing {string1InputValue} to {string2InputValue}");
+#endif
+            _cpu.Registers.AX = result ? 1 : 0;
         }
     }
 }
