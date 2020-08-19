dfc5f7077d1bbb1d322d48c9df61b6a79583ebf9
Eric Nusbaum <eric@enusbaum.com>
2020-08-19 14:00:19 -0400

    Added Documentation to IGlobalRoutine

--- MBBSEmu/HostProcess/GlobalRoutines/IGlobalRoutine.cs
+++ MBBSEmu/HostProcess/GlobalRoutines/IGlobalRoutine.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MBBSEmu.Memory;
+﻿using MBBSEmu.Memory;
 using MBBSEmu.Module;
 using MBBSEmu.Session;
+using System.Collections.Generic;
 
 namespace MBBSEmu.HostProcess.GlobalRoutines
 {
     public interface IGlobalRoutine
     {
+        /// <summary>
+        ///     Processes a Global Command and returns TRUE if the command was handled
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="channelNumber"></param>
+        /// <param name="sessions"></param>
+        /// <param name="modules"></param>
+        /// <returns></returns>
         bool ProcessCommand(byte[] command, ushort channelNumber, PointerDictionary<SessionBase> sessions,
             Dictionary<string, MbbsModule> modules);
     }
