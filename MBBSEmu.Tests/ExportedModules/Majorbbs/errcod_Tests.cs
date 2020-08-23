a567035ddd020c4e558c7a50edd7c2a77d576b9a
Eric Nusbaum <eric@enusbaum.com>
2020-08-22 20:56:43 -0400

    Added ERRCOD ordinal

---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/errcod_Tests.cs
@@ -0,0 +1,28 @@
+﻿using MBBSEmu.Memory;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class errcod_Tests : MajorbbsTestBase
+    {
+        private const int ERRCOD_ORDINAL = 193;
+
+        [Theory]
+        [InlineData(0x0)]
+        public void ClingoTests(ushort expectedValue)
+        {
+            //Reset State
+            Reset();
+
+            ExecuteApiTest(ERRCOD_ORDINAL, new List<IntPtr16>());
+
+            //Verify Results
+            var returnedPointer = ExecutePropertyTest(ERRCOD_ORDINAL);
+
+            var actualValue = mbbsEmuMemoryCore.GetWord(new IntPtr16(returnedPointer));
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
