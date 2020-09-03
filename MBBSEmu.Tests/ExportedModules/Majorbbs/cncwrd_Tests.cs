5be6e47175ff6000e9197bc9bb6e4824a1bf2da4
Eric Nusbaum <eric@enusbaum.com>
2020-09-03 16:14:14 -0400

    New Ordinals for NTHORDER
    - 5 new video buffer Ordinals (NOP)
    - `printf` that outputs to the console (stdout)
    - `gen_haskey` which is a generic "Has Key" implementation (Default to TRUE)
    - `cncwrd` which returns the next full word from the location of `NXTCMD`
    - Tests for `cncwrd`

---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/cncwrd_Tests.cs
@@ -0,0 +1,44 @@
+﻿using MBBSEmu.Memory;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class cncwrd_Tests : MajorbbsTestBase
+    {
+        private const int CNCWORD_ORDINAL = 130;
+
+        [Theory]
+        [InlineData("FIRST SECOND\0", 0, "FIRST", 6)]
+        [InlineData("FIRST SECOND\0", 6, "SECOND", 13)]
+        [InlineData("FIRST SECOND THIRD\0", 1, "IRST", 6)]
+        [InlineData("123456789012345678901234567890\0", 0, "12345678901234567890123456789", 30)] //Test Truncation
+        [InlineData("\0", 0, "", 0)]
+        public void cncchr_Test(string inputString, ushort nxtcmdStartingOffset, string expectedResult, ushort expectedNxtcmdOffset)
+        {
+            //Reset State
+            Reset();
+
+            //Set Input Values
+            var inputLength = (ushort)inputString.Length;
+
+            mbbsModule.Memory.SetArray("INPUT", Encoding.ASCII.GetBytes(inputString));
+            mbbsModule.Memory.SetWord("INPLEN", inputLength);
+
+            //Set nxtcmd
+            var currentNxtcmd = mbbsEmuMemoryCore.GetPointer("NXTCMD");
+            currentNxtcmd.Offset += nxtcmdStartingOffset;
+            mbbsEmuMemoryCore.SetPointer("NXTCMD", currentNxtcmd);
+
+            //Execute Test
+            ExecuteApiTest(CNCWORD_ORDINAL, new List<IntPtr16>());
+
+            //Verify Results
+            var expectedResultPointer = mbbsEmuMemoryCore.GetVariablePointer("INPUT");
+            expectedResultPointer.Offset += expectedNxtcmdOffset;
+            Assert.Equal(expectedResult, Encoding.ASCII.GetString(mbbsEmuMemoryCore.GetString(mbbsEmuCpuCore.Registers.DX, mbbsEmuCpuCore.Registers.AX, true)));
+            Assert.Equal(expectedResultPointer, mbbsEmuMemoryCore.GetPointer("NXTCMD"));
+        }
+    }
+}
