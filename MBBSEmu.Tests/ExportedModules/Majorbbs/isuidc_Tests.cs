fc67d821da6acaebf7eb83f30a5c0e46c5d2b8eb
Eric Nusbaum <eric@enusbaum.com>
2020-09-26 10:14:17 -0400

    New Ordinals
    - Added MAJORBBS.162 (DELBTV)
    - Added MAJORBBS.996 (GETBTVL), same as existing GETBTV with ignored extra parameter for lock type
    - Added MAJORBBS.737 (_8087) with tests
    - Added MAJORBBS.364 (ISUIDC) with tests

---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/isuidc_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class isuidc_Tests : ExportedModuleTestBase
+    {
+        private const int ISUIDC_ORDINAL = 364;
+
+        [Theory]
+        [InlineData(31, 0)]
+        [InlineData('!', 1)]
+        [InlineData('~', 1)]
+        [InlineData(127, 0)]
+        public void isuidc_Test(byte input, ushort expectedValue)
+        {
+            //Reset State
+            Reset();
+
+            //Allocate Variables to be Passed In
+
+
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, ISUIDC_ORDINAL, new List<ushort> {input});
+
+            //Verify Results
+            Assert.Equal(expectedValue, mbbsEmuCpuRegisters.AX);
+        }
+    }
+}
