6fb542de9be0c895cc0cf73be610bc592f5d0857
Eric P. Nusbaum <eric@enusbaum.com>
2020-10-25 15:06:58 -0400

    Fix for CNCALL
    - Fixed CNCALL to properlly return pointer for the remaining command (NXTCMD)
    - Properly sets NXTCMD to the NULL terminating INPUT
    - Added Unit Tests for CNCALL
    - Fix for CNCWRD test method name

--- MBBSEmu/HostProcess/ExportedModules/Majorbbs.cs
+++ MBBSEmu/HostProcess/ExportedModules/Majorbbs.cs
@@ -5526,12 +5526,14 @@ namespace MBBSEmu.HostProcess.ExportedModules
 
             var inputPointer = Module.Memory.GetVariablePointer("INPUT");
             var inputLength = Module.Memory.GetWord("INPLEN");
+            var nxtcmdPoiter = Module.Memory.GetPointer("NXTCMD");
 
-            var newNxtcmd = new IntPtr16(inputPointer.Segment, (ushort)(inputPointer.Offset + inputLength));
+            //Return current NXTCMD
+            Registers.SetPointer(nxtcmdPoiter);
 
+            //Set NXTCMD to the end of the INPUT
+            var newNxtcmd = new IntPtr16(inputPointer.Segment, (ushort)(inputPointer.Offset + inputLength - 1));
             Module.Memory.SetPointer("NXTCMD", newNxtcmd);
-
-            Registers.SetPointer(newNxtcmd);
         }
 
         /// <summary>
