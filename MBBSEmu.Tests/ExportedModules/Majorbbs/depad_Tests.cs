7a5adc7850f81c7c0794d13330362a386b551eb5
Jonathan Sider <jsider@gmail.com>
2020-10-30 23:21:21 -0500

    rmvwht, depad, stripb and zonkhl tests, fixes to majorbbs (#232)
    * rmvwht and zonkhl tests, fixes to majorbbs
    
    * Add trailing CR
    
    * Fixed trailing CRs and using
    
    * Fixed blank space
    
    * Updated Tests/Ordinals
    
    * Format
    
    * format
    
    * Formatting
    
    * Updated depad test
    
    * Update stripb()
    
    * Update stripb()
    
    * stripb tests
    
    * Update stripb declaration
    
    * add update AX check

---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/depad_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using MBBSEmu.Memory;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class depad_Tests : ExportedModuleTestBase
+    {
+        private const int DEPAD_ORDINAL = 164;
+
+        [Theory]
+        [InlineData("%   ", 3, "%\0")]
+        [InlineData("T    ", 4, "T\0")]
+        [InlineData("T ", 1, "T\0")]
+        [InlineData("TeSt  ", 2, "TeSt\0")]
+        [InlineData("TeSt", 0, "TeSt\0")]
+        [InlineData("", 0, "\0")]
+        public void DEPAD_Test(string inputString, ushort expected, string expectedString)
+        {
+            //Reset State
+            Reset();
+
+            var stringPointer = mbbsEmuMemoryCore.AllocateVariable("INPUT_STRING", (ushort)(inputString.Length + 1));
+            mbbsEmuMemoryCore.SetArray("INPUT_STRING", Encoding.ASCII.GetBytes(inputString));
+
+            //Execute Test
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, DEPAD_ORDINAL, new List<IntPtr16> { stringPointer });
+
+            //Verify Results
+            Assert.Equal(expected, mbbsEmuCpuRegisters.AX);
+            Assert.Equal(expectedString, Encoding.ASCII.GetString(mbbsEmuMemoryCore.GetString("INPUT_STRING")));
+        }
+    }
+}
