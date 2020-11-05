0d0a8191bf29f17f8cd0959a9cb45160e17140fb
Todd Jeffreys <todd@voidpointer.org>
2020-11-05 08:38:22 -0700

    Update bbsgen/bbsusr to match modern schema

--- MBBSEmu/Assets/bbsgen.db.sql
+++ MBBSEmu/Assets/bbsgen.db.sql
@@ -1,13 +1,13 @@
 CREATE TABLE metadata_t(record_length INTEGER NOT NULL, physical_record_length INTEGER NOT NULL, page_length INTEGER NOT NULL, variable_length_records INTEGER NOT NULL);
 
-INSERT INTO metadata_t(record_length, physical_record_length, page_length, variable_length_records) VALUES(55, 55, 1024, 0);
+INSERT INTO metadata_t(record_length, physical_record_length, page_length, variable_length_records) VALUES(55, 75, 1024, 1);
 
-CREATE TABLE keys_t(id INTEGER PRIMARY KEY, number INTEGER NOT NULL, segment INTEGER NOT NULL, attributes INTEGER NOT NULL, data_type INTEGER NOT NULL, offset INTEGER NOT NULL, length INTEGER NOT NULL, UNIQUE (number, segment));
+CREATE TABLE keys_t(id INTEGER PRIMARY KEY, number INTEGER NOT NULL, segment INTEGER NOT NULL, attributes INTEGER NOT NULL, data_type INTEGER NOT NULL, offset INTEGER NOT NULL, length INTEGER NOT NULL, null_value INTEGER NOT NULL, UNIQUE (number, segment));
 
-INSERT INTO keys_t(number, segment, attributes, data_type, offset, length) VALUES(0, 0, 51, 11, 0, 30);
-INSERT INTO keys_t(number, segment, attributes, data_type, offset, length) VALUES(0, 1, 35, 11, 30, 25);
+INSERT INTO keys_t(number, segment, attributes, data_type, offset, length, null_value) VALUES(0, 0, 307, 11, 0, 30, 0);
+INSERT INTO keys_t(number, segment, attributes, data_type, offset, length, null_value) VALUES(0, 1, 291, 11, 30, 25, 0);
 
-INSERT INTO keys_t(number, segment, attributes, data_type, offset, length) VALUES(1, 0, 35, 11, 30, 25);
+INSERT INTO keys_t(number, segment, attributes, data_type, offset, length, null_value) VALUES(1, 0, 291, 11, 30, 25, 0);
 
 CREATE TABLE data_t(id INTEGER PRIMARY KEY, data BLOB NOT NULL, key_0 STRING NOT NULL, key_1 STRING NOT NULL);
 
--- MBBSEmu/Assets/bbsusr.db.sql
+++ MBBSEmu/Assets/bbsusr.db.sql
@@ -2,9 +2,9 @@ CREATE TABLE metadata_t(record_length INTEGER NOT NULL, physical_record_length I
 
 INSERT INTO metadata_t(record_length, physical_record_length, page_length, variable_length_records) VALUES(338, 338, 1024, 0);
 
-CREATE TABLE keys_t(id INTEGER PRIMARY KEY, number INTEGER NOT NULL, segment INTEGER NOT NULL, attributes INTEGER NOT NULL, data_type INTEGER NOT NULL, offset INTEGER NOT NULL, length INTEGER NOT NULL, UNIQUE (number, segment));
+CREATE TABLE keys_t(id INTEGER PRIMARY KEY, number INTEGER NOT NULL, segment INTEGER NOT NULL, attributes INTEGER NOT NULL, data_type INTEGER NOT NULL, offset INTEGER NOT NULL, length INTEGER NOT NULL, null_value INTEGER NOT NULL, UNIQUE (number, segment));
 
-INSERT INTO keys_t(number, segment, attributes, data_type, offset, length) VALUES(0, 0, 0, 11, 0, 30);
+INSERT INTO keys_t(number, segment, attributes, data_type, offset, length, null_value) VALUES(0, 0, 288, 11, 0, 30, 0);
 
 CREATE TABLE data_t(id INTEGER PRIMARY KEY, data BLOB NOT NULL, key_0 STRING NOT NULL);
 
