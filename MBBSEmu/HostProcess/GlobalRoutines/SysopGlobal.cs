33827b85ee1358054b66d86d73da75bdc5c47e62
Jonathan Sider <jsider@gmail.com>
2020-11-14 07:50:44 -0600

    Formatting

--- MBBSEmu/HostProcess/GlobalRoutines/SysopGlobal.cs
+++ MBBSEmu/HostProcess/GlobalRoutines/SysopGlobal.cs
@@ -1,4 +1,6 @@
-﻿using MBBSEmu.Database.Repositories.Account;
+﻿using MBBSEmu.Btrieve;
+using MBBSEmu.Btrieve.Enums;
+using MBBSEmu.Database.Repositories.Account;
 using MBBSEmu.Database.Repositories.AccountKey;
 using MBBSEmu.Extensions;
 using MBBSEmu.Memory;
@@ -9,9 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MBBSEmu.Btrieve;
-using MBBSEmu.Btrieve.Enums;
-using MBBSEmu.HostProcess.Structs;
 
 namespace MBBSEmu.HostProcess.GlobalRoutines
 {
