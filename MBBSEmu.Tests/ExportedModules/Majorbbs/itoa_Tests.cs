28e7617af9aa508d07ea8427a3ec6f3264dba8f7
Jonathan Sider <jsider@gmail.com>
2020-11-22 09:23:35 -0600

    Removed redundant variable

--- MBBSEmu.Tests/ExportedModules/Majorbbs/itoa_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/itoa_Tests.cs
@@ -9,21 +9,21 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         private const int ITOA_ORDINAL = 366;
 
         [Theory]
-        [InlineData(4, "STRDST", 10, "4")]
-        [InlineData(500, "STRDST", 10, "500")]
-        [InlineData(8500, "STRDST", 8, "20464")]
-        [InlineData(8500, "STRDST", 16, "2134")]
-        [InlineData(5, "STRDST", 2, "101")]
-        [InlineData(1000, "STRDST", 16, "3e8")]
-        [InlineData(1000, "STRDST", 2, "1111101000")]
-        public void itoa_Test(ushort intConvert, string strDestination, ushort baseInt, string expected)
+        [InlineData(4, 10, "4")]
+        [InlineData(500, 10, "500")]
+        [InlineData(8500, 8, "20464")]
+        [InlineData(8500, 16, "2134")]
+        [InlineData(5, 2, "101")]
+        [InlineData(1000, 16, "3e8")]
+        [InlineData(1000, 2, "1111101000")]
+        public void itoa_Test(ushort intConvert, ushort baseInt, string expected)
         {
             //Reset State
             Reset();
 
             //Set Argument Values to be Passed In
             var destinationStringPointer = mbbsEmuMemoryCore.AllocateVariable("STRDST", (ushort)(expected.Length + 1));
-            mbbsEmuMemoryCore.SetArray("STRDST", Encoding.ASCII.GetBytes(strDestination));
+            mbbsEmuMemoryCore.SetArray("STRDST", Encoding.ASCII.GetBytes("STRDST"));
 
             //Execute Test
             ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, ITOA_ORDINAL,
