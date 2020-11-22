2a57bffbc700abb536fcfbad37ed6a1cee96d02f
Jonathan Sider <jsider@gmail.com>
2020-11-22 16:12:44 -0600

    SortStrgs

--- MBBSEmu.Tests/ExportedModules/Majorbbs/sortstgs_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/sortstgs_Tests.cs
@@ -1,29 +1,40 @@
-﻿using MBBSEmu.Memory;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
 namespace MBBSEmu.Tests.ExportedModules.Majorbbs
 {
-    ///     Signature: void sortstgs(char *stgs[],int num) 558;
     public class sortstgs_Tests : ExportedModuleTestBase
     {
         private const int SORTSTGS_ORDINAL = 558;
 
         [Theory]
-        [InlineData(new [] { "This", "Is", "an", "array" }, 4, new [] { "This", "Is", "an", "array" })]
+        [InlineData(new[] {"This", "is", "an", "array"}, 4, new[] {"an", "array", "is", "This"})]
         public void sortstgs_Test(string[] inputArray, ushort numElements, string[] expectedArray)
         {
             //Reset State
             Reset();
 
-            var stringPointer = mbbsEmuMemoryCore.AllocateVariable("INPUT_STRING", (ushort)(inputArray.Length + 1));
-            mbbsEmuMemoryCore.SetArray("INPUT_STRING", Encoding.ASCII.GetBytes(inputArray));
+            var stringPointer = mbbsEmuMemoryCore.AllocateVariable("INPUT_STRING", (ushort) (inputArray.Length + 1));
+
+            foreach (var t in inputArray)
+                mbbsEmuMemoryCore.SetArray("INPUT_STRING", Encoding.ASCII.GetBytes(t));
 
             //Execute Test
-            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, SORTSTGS_ORDINAL, new List<IntPtr16> { stringPointer });
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, SORTSTGS_ORDINAL,
+                new List<ushort>
+                {
+                    stringPointer.Offset,
+                    stringPointer.Segment,
+                    numElements
+                });
 
+
+            var dstArray = mbbsEmuMemoryCore.GetArray("INPUT_STRING", numElements);
+            
             //Verify Results
-            Assert.Equal(expectedArray, Encoding.ASCII.GetString(mbbsEmuMemoryCore.GetString("INPUT_STRING")));
+            //Assert.Equal(expectedArray, dstArray.ToArray());
+            //Assert.Equal(0, dstArray.ToArray());
         }
     }
+}
