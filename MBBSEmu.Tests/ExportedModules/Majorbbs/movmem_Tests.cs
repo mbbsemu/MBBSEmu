b6a1bb97db54c24cf06158f1f4ec784988d6fe24
Jonathan Sider <jsider@gmail.com>
2020-11-25 17:06:09 -0600

    Added momvmem test for same pointer

--- MBBSEmu.Tests/ExportedModules/Majorbbs/movmem_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/movmem_Tests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace MBBSEmu.Tests.ExportedModules.Majorbbs
@@ -9,22 +8,54 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         private const int MOVMEM_ORDINAL = 420;
 
         [Theory]
-        [InlineData(3, new byte[] { 0x4, 0x4, 0x2 })]
-        [InlineData(1, new byte[] { 0xE })]
-        [InlineData(2, new byte[] { 0xD, 0xA })]
-        [InlineData(5, new byte[] { 0xD, 0xA, 0xD, 0xD, 0x2 })]
-        [InlineData(0, new byte[] { })]
-        public void movmem_Test(ushort moveLength, byte[] expected)
+        [InlineData(new byte[] { 0x4, 0x4, 0x2 })]
+        [InlineData(new byte[] { 0xE })]
+        [InlineData(new byte[] { 0xD, 0xA })]
+        [InlineData(new byte[] { 0xD, 0xA, 0xD, 0xD, 0x2 })]
+        [InlineData(new byte[] { })]
+        public void movmem_Test(byte[] expected)
         {
             //Reset State
             Reset();
 
             //Set Argument Values to be Passed In
-            var srcPointer = mbbsEmuMemoryCore.AllocateVariable("SRC", moveLength);
+            var srcPointer = mbbsEmuMemoryCore.AllocateVariable("SRC", (ushort) expected.Length);
             mbbsEmuMemoryCore.SetArray("SRC", expected);
 
-            var dstPointer = mbbsEmuMemoryCore.AllocateVariable("DST", (ushort)(moveLength + 1));
+            var dstPointer = mbbsEmuMemoryCore.AllocateVariable("DST", (ushort)(expected.Length + 1));
 
+            //Execute Test
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, MOVMEM_ORDINAL,
+                new List<ushort>
+                {
+                    srcPointer.Offset,
+                    srcPointer.Segment,
+                    dstPointer.Offset,
+                    dstPointer.Segment,
+                    (ushort) expected.Length
+                });
+
+            //Verify Results
+            var dstArray = mbbsEmuMemoryCore.GetArray("DST", (ushort) expected.Length);
+
+            Assert.Equal(0, mbbsEmuCpuRegisters.AX);
+            Assert.Equal(0, mbbsEmuCpuRegisters.DX);
+            Assert.Equal(expected, dstArray.ToArray());
+        }
+
+        [Theory]
+        [InlineData(1, new byte[] { 0x54, 0x45, 0x53, 0x54 }, new byte[] { 0x45, 0x53, 0x54, 0x54 })]
+        public void movmem_samepointer_Test(ushort moveLength, byte[] source, byte[] expected)
+        {
+            //Reset State
+            Reset();
+
+            //Set Argument Values to be Passed In
+            var srcPointer = mbbsEmuMemoryCore.AllocateVariable("POINTER", (ushort)source.Length);
+            mbbsEmuMemoryCore.SetArray("POINTER", source);
+
+            var dstPointer = mbbsEmuMemoryCore.GetVariablePointer("POINTER");
+            
             //Execute Test
             ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, MOVMEM_ORDINAL,
                 new List<ushort>
@@ -37,8 +68,10 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
                 });
 
             //Verify Results
-            var dstArray = mbbsEmuMemoryCore.GetArray("DST", moveLength);
+            var dstArray = mbbsEmuMemoryCore.GetArray("POINTER", (ushort)expected.Length);
 
+            Assert.Equal(0, mbbsEmuCpuRegisters.AX);
+            Assert.Equal(0, mbbsEmuCpuRegisters.DX);
             Assert.Equal(expected, dstArray.ToArray());
         }
     }
