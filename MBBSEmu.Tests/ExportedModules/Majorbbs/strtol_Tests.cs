6481e3487cc66a33cbfc507fe1f864edaa3722af
Jonathan Sider <jsider@gmail.com>
2020-12-05 20:03:18 -0600

    Added 1 more test for 0xFFFFF

--- MBBSEmu.Tests/ExportedModules/Majorbbs/strtol_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/strtol_Tests.cs
@@ -16,6 +16,7 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         [InlineData("78 A", 3, 10, 78)]
         [InlineData("0x23CACE A very big number", 9, 16, 2345678)]
         [InlineData("-2345678 A very big number", 9, 10, -2345678)]
+        [InlineData("0xFFFFF is my favorite number", 8, 16, 1048575)]
         [InlineData("", 0, 10, 0)]
         [InlineData("ThereIsNoNumberHere", 0, 10, 0)]
         public void strtol_Test(string srcString, ushort ushortPtr, ushort numBase, int expectedValue)
