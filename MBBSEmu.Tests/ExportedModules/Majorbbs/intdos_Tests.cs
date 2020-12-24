816c72122e60004b256d11d304954fd81859ef7c
Todd Jeffreys <todd@voidpointer.org>
2020-12-24 14:48:35 -0700

    Get intdos tests passing

--- MBBSEmu.Tests/ExportedModules/Majorbbs/intdos_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/intdos_Tests.cs
@@ -18,18 +18,24 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
 
             //Set Argument Values to be Passed In
             var testRegisters = new CpuRegisters();
-            var testRegistersPointer = testRegisters.GetPointer();
-
             testRegisters.AH = 0x2A;
 
-            mbbsEmuCpuRegisters.FromRegs(mbbsEmuMemoryCore.GetArray(testRegistersPointer, 16));
-
+            //Allocate some memory to hold the test data
+            var testRegistersArrayData = testRegisters.ToRegs();
+            var testRegistersPointer = mbbsEmuMemoryCore.AllocateVariable(null, (ushort)testRegistersArrayData.Length);
+            mbbsEmuMemoryCore.SetArray(testRegistersPointer, testRegistersArrayData);
 
             //Execute Test
             ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, INTDOS_ORDINAL, new List<IntPtr16> { testRegistersPointer, testRegistersPointer });
 
+            //Data returned in memory, so we need to reload testRegisters from memory
+            testRegisters.FromRegs(mbbsEmuMemoryCore.GetArray(testRegistersPointer, (ushort)testRegistersArrayData.Length));
+
             //Verify Results
-            Assert.Equal((byte) DateTime.Now.Month, mbbsEmuCpuRegisters.DL);
+            Assert.Equal(DateTime.Now.Day, testRegisters.DL);
+            Assert.Equal(DateTime.Now.Month, testRegisters.DH);
+            Assert.Equal(DateTime.Now.Year, testRegisters.CX);
+            Assert.Equal((byte)DateTime.Now.DayOfWeek, testRegisters.AL);
         }
     }
 }
