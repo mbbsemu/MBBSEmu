50755c2c8080ac0f3d1262a45adc51330695f7fc
Jonathan Sider <jsider@gmail.com>
2020-12-27 16:12:12 -0600

    Fixed timezone

--- MBBSEmu.Tests/ExportedModules/Majorbbs/dostounix_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/dostounix_Tests.cs
@@ -11,19 +11,21 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         private const int DOSTOUNIX_ORDINAL = 173;
 
         [Theory]
-        [InlineData(1, 1, 2000, 23, 0, 1, 946789201)]
-        [InlineData(2, 2, 2010, 1, 30, 30, 1265095830)]
-        [InlineData(3, 3, 2020, 8, 45, 15, 1583246715)]
-        [InlineData(4, 22, 2035, 11, 0, 59, 2060870459)]
-        [InlineData(12, 31, 1980, 20, 59, 1, 347165941)]
-        [InlineData(1, 18, 2038, 2, 14, 7, 2147415247)]
-        [InlineData(1, 1, 1970, 0, 0, 0, 21600)]
-        public void dostounix_Test(int month, int day, int year, int hour, int minute, int second, int expectedUnixDate)
+        [InlineData(1, 1, 1970, 0, 0, 0)]
+        [InlineData(1, 1, 1975, 0, 0, 1)]
+        [InlineData(1, 1, 2000, 23, 0, 1)]
+        [InlineData(2, 2, 2010, 1, 30, 30)]
+        [InlineData(3, 3, 2020, 8, 45, 15)]
+        [InlineData(4, 22, 2035, 11, 0, 59)]
+        [InlineData(12, 31, 1980, 20, 59, 1)]
+        [InlineData(1, 18, 2038, 2, 14, 7)]
+        public void dostounix_Test(int month, int day, int year, int hour, int minute, int second)
         {
             //Reset State
             Reset();
 
             fakeClock.Now = new DateTime(year, month, day, hour, minute, second);
+            var expectedUnixDate = ((DateTimeOffset)fakeClock.Now).ToUnixTimeSeconds();
 
             var inputDateStruct = new DateStruct(fakeClock.Now);
             var inputTimeStruct = new TimeStruct(fakeClock.Now);
@@ -39,8 +41,7 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
             ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, DOSTOUNIX_ORDINAL, new List<FarPtr> { dateStringPointer, timeStringPointer });
 
             //Verify Results
-            Assert.Equal(expectedUnixDate >> 16, mbbsEmuCpuRegisters.DX);
-            Assert.Equal(expectedUnixDate & 0xFFFF, mbbsEmuCpuRegisters.AX);
+            Assert.Equal(expectedUnixDate, mbbsEmuCpuRegisters.GetLong());
         }
     }
 }
