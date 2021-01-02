2044caac10236290842907607d0d1ad96f9d681e
Todd Jeffreys <todd@voidpointer.org>
2021-01-02 09:27:09 -0700

    Add a test case to stzcat for when the buffer has plenty of space.

--- MBBSEmu.Tests/ExportedModules/Majorbbs/stzcat_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/stzcat_Tests.cs
@@ -13,30 +13,35 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         private const int STZCAT_ORDINAL = 905;
 
         [Theory]
-        [InlineData("12345", "12345", 7, "123451")]
-        [InlineData("12345", "12345", 4, "123")]
+        [InlineData("12345", "12345", 7, "123451\0")]
+        [InlineData("12345", "12345", 4, "123\0")]
         [InlineData("12345", "12345", 0, "")]
-        [InlineData("12345", "12345", 11, "1234512345")]
+        [InlineData("12345", "12345", 11, "1234512345\0")]
+        [InlineData("12345", "12345", 16, "1234512345\0\0\0\0\0\0")]
         [InlineData("", "", 0, "")]
-        public void stzcat_Test(string destination, string src, ushort count, string expected)
+        public void stzcat_Test(string destination, string src, ushort dstLength, string expected)
         {
             //Reset State
             Reset();
 
             //Set Argument Values to be Passed In
-            var destinationStringPointer = mbbsEmuMemoryCore.AllocateVariable("DST", (ushort)(destination.Length + src.Length + 1));
-            mbbsEmuMemoryCore.SetArray("DST", Encoding.ASCII.GetBytes(destination));
+            var destinationStringPointer = mbbsEmuMemoryCore.AllocateVariable("DST", (ushort)dstLength);
+            mbbsEmuMemoryCore.FillArray(destinationStringPointer, (ushort)dstLength, 0xFF);
+
+            mbbsEmuMemoryCore.SetArray(destinationStringPointer, Encoding.ASCII.GetBytes(destination));
+            mbbsEmuMemoryCore.SetByte(destinationStringPointer + destination.Length, 0);
 
             var sourceStringPointer = mbbsEmuMemoryCore.AllocateVariable("SRC", (ushort)(src.Length + 1));
-            mbbsEmuMemoryCore.SetArray("SRC", Encoding.ASCII.GetBytes(src));
+            mbbsEmuMemoryCore.SetArray(sourceStringPointer, Encoding.ASCII.GetBytes(src));
+            mbbsEmuMemoryCore.SetByte(sourceStringPointer + src.Length, 0);
 
             //Execute Test
-            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, STZCAT_ORDINAL, new List<ushort> { destinationStringPointer.Offset, destinationStringPointer.Segment, sourceStringPointer.Offset, sourceStringPointer.Segment, count });
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, STZCAT_ORDINAL, new List<ushort> { destinationStringPointer.Offset, destinationStringPointer.Segment, sourceStringPointer.Offset, sourceStringPointer.Segment, dstLength });
 
             //Verify Results
             Assert.Equal(destinationStringPointer.Segment, mbbsEmuCpuRegisters.DX);
             Assert.Equal(destinationStringPointer.Offset, mbbsEmuCpuRegisters.AX);
-            Assert.Equal(expected, Encoding.ASCII.GetString(mbbsEmuMemoryCore.GetString("DST", true)));
+            Assert.Equal(expected, Encoding.ASCII.GetString(mbbsEmuMemoryCore.GetArray("DST", dstLength)));
         }
     }
 }
