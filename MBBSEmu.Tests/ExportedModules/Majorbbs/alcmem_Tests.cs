93c12565d8989ed295c6bb857608cf9bd53acdca
Jonathan Sider <jsider@gmail.com>
2021-01-31 20:28:55 -0600

    add alcmem(), alczer() tests

---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/alcmem_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class alcmem_Tests : ExportedModuleTestBase
+    {
+        private const int ALCMEM_ORDINAL = 65;
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(72)]
+        [InlineData(45)]
+        [InlineData(0)]
+        public void alcmem_Test(ushort numBytes)
+        {
+            //Reset State
+            Reset();
+
+            //Execute Test
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, ALCMEM_ORDINAL, new List<ushort> { numBytes });
+
+            //Verify Results
+            var expected = new byte[numBytes];
+
+            for (var i = 0; i < numBytes; i++)
+                expected[i] = 0x0;
+
+            var dstArray = mbbsEmuMemoryCore.GetArray(mbbsEmuCpuRegisters.GetPointer(), numBytes);
+
+            //Verify Results
+            Assert.Equal(expected, dstArray.ToArray());
+        }
+    }
+}
---
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/alczer_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace MBBSEmu.Tests.ExportedModules.Majorbbs
+{
+    public class alczer_Tests : ExportedModuleTestBase
+    {
+        private const int ALCZER_ORDINAL = 68;
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(72)]
+        [InlineData(45)]
+        [InlineData(0)]
+        public void alczer_Test(ushort numBytes)
+        {
+            //Reset State
+            Reset();
+
+            //Execute Test
+            ExecuteApiTest(HostProcess.ExportedModules.Majorbbs.Segment, ALCZER_ORDINAL, new List<ushort> { numBytes });
+
+            //Verify Results
+            var expected = new byte[numBytes];
+
+            for (var i = 0; i < numBytes; i++)
+                expected[i] = 0x0;
+
+            var dstArray = mbbsEmuMemoryCore.GetArray(mbbsEmuCpuRegisters.GetPointer(), numBytes);
+
+            //Verify Results
+            Assert.Equal(expected, dstArray.ToArray());
+        }
+    }
+}
