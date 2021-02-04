931de960d3bbbc6fdb4c4e1b45809c5a6ab198fe
Eric P. Nusbaum <eric@enusbaum.com>
2021-02-03 22:31:10 -0500

    Initial Text Variable Service (#395)

---
+++ MBBSEmu/TextVariables/ITextVariableService.cs
@@ -0,0 +1,33 @@
+﻿namespace MBBSEmu.TextVariables
+{
+    public interface ITextVariableService
+    {
+        /// <summary>
+        ///     Sets the specified variable
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        void SetVariable(string name, TextVariable.TextVariableValueDelegate value);
+
+        /// <summary>
+        ///     Gets the specified variable value by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        string GetVariableByName(string name);
+
+        /// <summary>
+        ///     Gets the specified variable value by index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        string GetVariableByIndex(int index);
+
+        /// <summary>
+        ///     Gets the index of the specified variable by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        int GetVariableIndex(string name);
+    }
+}
