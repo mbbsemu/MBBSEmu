3eb4007461642417149d500b05614020941cc7a9
Todd Jeffreys <todd@voidpointer.org>
2021-02-24 09:29:37 -0700

    Finish the MemoryAllocator implementation :)

--- MBBSEmu.Tests/Memory/MemoryAllocator_Tests.cs
+++ MBBSEmu.Tests/Memory/MemoryAllocator_Tests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using MBBSEmu.DependencyInjection;
 using MBBSEmu.Memory;
 using NLog;
+using System.Collections.Generic;
 using System;
 using Xunit;
 
@@ -18,7 +19,8 @@ namespace MBBSEmu.Tests.Memory
     {
       var allocator = new MemoryAllocator(_logger, new FarPtr(SEGMENT, 0), 0x10000);
       allocator.BasePointer.Should().Be(new FarPtr(SEGMENT, 0));
-      allocator.Size.Should().Be(0x10000);
+      allocator.Capacity.Should().Be(0x10000);
+      allocator.RemainingBytes.Should().Be(0x10000);
     }
 
     [Fact]
@@ -41,6 +43,7 @@ namespace MBBSEmu.Tests.Memory
       var allocator = new MemoryAllocator(_logger, new FarPtr(SEGMENT, 0), 0x10000);
 
       allocator.Malloc(0).Should().Be(FarPtr.Empty);
+      allocator.RemainingBytes.Should().Be(0x10000);
     }
 
     [Fact]
@@ -60,6 +63,7 @@ namespace MBBSEmu.Tests.Memory
       memory.Offset.Should().Be(2);
 
       allocator.FreeBlocks.Should().Be(1);
+      allocator.RemainingBytes.Should().Be(0xFFFE - 16);
     }
 
     [Fact]
@@ -72,6 +76,12 @@ namespace MBBSEmu.Tests.Memory
       memory.Offset.Should().Be(2);
 
       allocator.FreeBlocks.Should().Be(0);
+      allocator.RemainingBytes.Should().Be(0);
+
+      // and free it to make sure everything is back in place
+      allocator.Free(memory);
+      allocator.FreeBlocks.Should().Be(1);
+      allocator.RemainingBytes.Should().Be(0xFFFE);
     }
 
     [Fact]
@@ -88,6 +98,7 @@ namespace MBBSEmu.Tests.Memory
       memory.Offset.Should().Be(18);
 
       allocator.FreeBlocks.Should().Be(1);
+      allocator.RemainingBytes.Should().Be(0xFFFE - 32);
     }
 
     [Fact]
@@ -106,10 +117,11 @@ namespace MBBSEmu.Tests.Memory
       memory.Offset.Should().Be(2);
 
       allocator.FreeBlocks.Should().Be(1);
+      allocator.RemainingBytes.Should().Be(0xFFFE - 16);
     }
 
     [Fact]
-    public void AllocationAndFreeOutOfOrderRepeat()
+    public void AllocationAndFreeOutOfOrderRepeatFirstOrder()
     {
       var allocator = new MemoryAllocator(_logger, new FarPtr(SEGMENT, 2), 0xFFFE);
 
@@ -138,6 +150,89 @@ namespace MBBSEmu.Tests.Memory
 
       // free final one, should compact
       allocator.Free(memory3);
+      allocator.FreeBlocks.Should().Be(1);
+
+      allocator.RemainingBytes.Should().Be(0xFFFE);
+    }
+
+    [Fact]
+    public void AllocationAndFreeOutOfOrderRepeatSecondOrder()
+    {
+      var allocator = new MemoryAllocator(_logger, new FarPtr(SEGMENT, 2), 0xFFFE);
+
+      var memory1 = allocator.Malloc(16);
+      memory1.Segment.Should().Be(SEGMENT);
+      memory1.Offset.Should().Be(2);
+
+      var memory2 = allocator.Malloc(256);
+      memory2.Segment.Should().Be(SEGMENT);
+      memory2.Offset.Should().Be(18);
+
+      var memory3 = allocator.Malloc(16);
+      memory3.Segment.Should().Be(SEGMENT);
+      memory3.Offset.Should().Be(256 + 18);
+
+      allocator.FreeBlocks.Should().Be(1);
+
+      // free middle one, nothing should be able to be compacted
+      allocator.Free(memory2);
+
+      allocator.FreeBlocks.Should().Be(2);
+
+      // free last one, should compact
+      allocator.Free(memory3);
+      allocator.FreeBlocks.Should().Be(1);
+
+      // free first one, should compact
+      allocator.Free(memory1);
+      allocator.FreeBlocks.Should().Be(1);
+
+      allocator.RemainingBytes.Should().Be(0xFFFE);
+    }
+
+    private static void FreeRandom(Random random, List<FarPtr> allocatedMemory, MemoryAllocator allocator)
+    {
+      var index = random.Next(allocatedMemory.Count);
+
+      allocator.Free(allocatedMemory[index]);
+
+      allocatedMemory.RemoveAt(index);
+    }
+
+    [Fact]
+    public void AllocateEverything()
+    {
+      var random = new Random();
+      var memory = new List<FarPtr>();
+      var allocator = new MemoryAllocator(_logger, new FarPtr(SEGMENT, 2), 0xFFFE);
+      // allocate all the memory
+      while (allocator.RemainingBytes > 0)
+      {
+        var size = random.Next(Math.Min(256, (int)allocator.RemainingBytes)) + 1;
+        var ptr = allocator.Malloc((ushort)size);
+        // could be null due to fragmentation of the memory space, so just skip and move on to another size
+        if (ptr.IsNull())
+          continue;
+
+        memory.Add(ptr);
+
+        // randomly free in the middle of the allocations to fragment memory space on purpose
+        if (random.Next(4) == 0)
+        {
+          FreeRandom(random, memory, allocator);
+        }
+      }
+
+      // ensure future requests fail since we're out of memory
+      allocator.Malloc(0).Should().Be(FarPtr.Empty);
+      allocator.Malloc(1).Should().Be(FarPtr.Empty);
+
+      while (memory.Count > 0)
+      {
+         FreeRandom(random, memory, allocator);
+      }
+
+      allocator.RemainingBytes.Should().Be(0xFFFE);
       allocator.FreeBlocks.Should().Be(1);
     }
   }
--- MBBSEmu/Memory/MemoryAllocator.cs
+++ MBBSEmu/Memory/MemoryAllocator.cs
@@ -6,49 +6,113 @@ using System;
 
 namespace MBBSEmu.Memory
 {
+  /// <summary>
+  ///   A Memory Allocator that implements malloc/free.
+  /// </summary>
   public class MemoryAllocator
   {
+    /// <summary>
+    ///   Represents a block of memory.
+    /// </summary>
     private class MemoryBlock
     {
+      /// <summary>
+      ///   Offset within the segment where this memory begins.
+      /// </summary>
       public ushort Offset { get; set; }
+      /// <summary>
+      ///   Size in bytes of this memory block.
+      /// </summary>
       public uint Size { get; set; }
 
+      /// <summary>
+      ///   Offset within the segment where this memory ends.
+      /// </summary>
       public uint EndOffset { get => Offset + Size; }
     }
 
+    /// <summary>
+    ///   List of free memory blocks we currently have.
+    /// </summary>
     private LinkedList<MemoryBlock> _freeBlocks = new();
+    /// <summary>
+    ///   Stores all of our allocated memory blocks.
+    ///
+    ///   Key is the pointer, Value is the size of the allocated memory block.
+    /// </summary>
     private ConcurrentDictionary<FarPtr, uint> _allocatedBlocks = new();
 
+    /// <summary>
+    ///   Lowest memory address that this allocator can return as a valid pointer.
+    /// </summary>
     public FarPtr BasePointer { get; init; }
 
-    public uint Size { get; init; }
+    /// <summary>
+    ///   Total capacity in bytes that this memory allocator can allocate.
+    ///
+    ///   Could allocate less than this value depending on the fragmentation level.
+    /// </summary>
+    public uint Capacity { get; init; }
+
+    /// <summary>
+    ///   How many remaining bytes are available to be allocated.
+    /// </summary>
+    public uint RemainingBytes { get; set; }
 
     public ILogger Logger { get; init; }
 
+    /// <summary>
+    ///   The number of free blocks being tracked.
+    ///
+    ///   Only used for testing.
+    /// </summary>
     public int FreeBlocks { get => _freeBlocks.Count; }
 
-    public MemoryAllocator(ILogger logger, FarPtr basePointer, uint size)
+    /// <summary>
+    ///   Constructs a new MemoryAllocator.
+    ///
+    ///   basePointer + capacity must be smaller than a segment size, to ensure it will not overflow
+    ///   allocations into the next segment.
+    /// </summary>
+    /// <param name="logger">logger</param>
+    /// <param name="basePointer">lowest memory address returnable from malloc</param>
+    /// <param name="capacity">size in bytes that this allocator can draw memory from</param>
+    public MemoryAllocator(ILogger logger, FarPtr basePointer, uint capacity)
     {
-      var endOffset = basePointer.Offset + size;
+      var endOffset = basePointer.Offset + capacity;
       if (endOffset > 0x10000)
         throw new ArgumentException("pointer + size overflows segment");
 
       BasePointer = basePointer;
-      Size = size;
+      Capacity = capacity;
+      RemainingBytes = capacity;
 
-      _freeBlocks.AddFirst(new MemoryBlock() { Offset = basePointer.Offset, Size = size });
+      _freeBlocks.AddFirst(new MemoryBlock() { Offset = basePointer.Offset, Size = capacity });
     }
 
+    /// <summary>
+    ///   Allocates a block of memory of size size.
+    ///
+    ///   Returns NULL if size is 0 or no memory is available.
+    /// </summary>
+    /// <param name="size">size in bytes to allocate.</param>
+    /// <returns></returns>
     public FarPtr Malloc(ushort size)
     {
-      if (size == 0)
+      if (size == 0 || _freeBlocks.Count == 0)
+      {
+        Logger?.Warn("Failed to allocate memory of size {size} since we have no free blocks, or bad argument.");
         return FarPtr.Empty;
+      }
 
       var foundBlock = _freeBlocks.EnumerateNodes()
           .Where(memoryBlock => memoryBlock.Value.Size >= size)
-          .Aggregate((curMin, memoryBlock) => (curMin == null || (memoryBlock.Value.Size < curMin.Value.Size) ? memoryBlock : curMin));
+          .Aggregate((LinkedListNode<MemoryBlock>)null, (curMin, memoryBlock) => (curMin == null || (memoryBlock.Value.Size < curMin.Value.Size) ? memoryBlock : curMin));
       if (foundBlock == null)
+      {
+        Logger?.Warn("Failed to allocate memory of size {size} since we can't find a large enough free block.");
         return FarPtr.Empty;
+      }
 
       var returnPtr = new FarPtr(BasePointer.Segment, foundBlock.Value.Offset);
       _allocatedBlocks[returnPtr] = size;
@@ -63,9 +127,14 @@ namespace MBBSEmu.Memory
         _freeBlocks.Remove(foundBlock);
       }
 
+      RemainingBytes -= size;
       return returnPtr;
     }
 
+    /// <summary>
+    ///   Frees a block of memory previously allocated with Malloc.
+    /// </summary>
+    /// <param name="ptr">block of memory previous allocated with Malloc</param>
     public void Free(FarPtr ptr)
     {
       if (ptr == FarPtr.Empty)
@@ -77,44 +146,59 @@ namespace MBBSEmu.Memory
         return;
       }
 
+      RemainingBytes += size;
+
       // see if we can remerge
-      var node = _freeBlocks.EnumerateNodes().Where(node => node.Value.Offset == ptr.Offset + size).FirstOrDefault();
+      var node = _freeBlocks.EnumerateNodes().Where(node => node.Value.Offset == ptr.Offset + size || node.Value.EndOffset == ptr.Offset).FirstOrDefault();
       if (node != null)
       {
-        node.Value.Offset = ptr.Offset;
+        // if the adjacent free block is immediately after the freed memory, change the adjacent
+        // free block starting offset (lowers it)
+        if (node.Value.Offset == ptr.Offset + size)
+          node.Value.Offset = ptr.Offset;
+
         node.Value.Size += size;
         Compact(node);
         return;
       }
 
-      node = _freeBlocks.EnumerateNodes().Where(node => node.Value.Offset == ptr.Offset - size).FirstOrDefault();
-      if (node != null)
-      {
-        node.Value.Size += size;
-        Compact(node);
-        return;
-      }
-
-      _freeBlocks.AddFirst(new MemoryBlock() { Offset = ptr.Offset, Size = size});
+      _freeBlocks.AddFirst(new MemoryBlock() { Offset = ptr.Offset, Size = size });
     }
-  
-    private LinkedListNode<MemoryBlock> UpdateAndRemoveNode(LinkedListNode<MemoryBlock> liveNode, LinkedListNode<MemoryBlock> deadNode) 
+
+    private LinkedListNode<MemoryBlock> MergeNodesAndRemoveDeadNode(LinkedListNode<MemoryBlock> liveNode, LinkedListNode<MemoryBlock> deadNode)
     {
       liveNode.Value.Size += deadNode.Value.Size;
       _freeBlocks.Remove(deadNode);
       return liveNode;
     }
 
-    // looks for ways to compact updatedBlock
+    /// <summary>
+    ///   Compacts _freeBlocks as best we can.
+    /// </summary>
+    /// <param name="updatedBlock">the memory block that caused the compaction</param>
     private void Compact(LinkedListNode<MemoryBlock> updatedBlock)
     {
       while (_freeBlocks.Count > 1)
       {
-        // look for nodes that have endoffset = updatedBlock
+        // look for nodes that join each other
         var node = _freeBlocks.EnumerateNodes().Where(node => node.Value.EndOffset == updatedBlock.Value.Offset || updatedBlock.Value.EndOffset == node.Value.Offset).FirstOrDefault();
-        if (node != null) 
+        if (node != null)
         {
-          updatedBlock = UpdateAndRemoveNode(node, updatedBlock);
+          LinkedListNode<MemoryBlock> liveNode;
+          LinkedListNode<MemoryBlock> deadNode;
+          // live node always has the lowest offset
+          if (node.Value.Offset < updatedBlock.Value.Offset)
+          {
+            liveNode = node;
+            deadNode = updatedBlock;
+          }
+          else
+          {
+            liveNode = updatedBlock;
+            deadNode = node;
+          }
+          updatedBlock = MergeNodesAndRemoveDeadNode(liveNode, deadNode);
+          // we've updated another block, so try to compact again
           continue;
         }
 
