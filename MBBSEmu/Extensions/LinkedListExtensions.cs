419639994a03edffbfcb1f12de416831ab449f54
Todd Jeffreys <todd@voidpointer.org>
2021-02-24 09:32:41 -0700

    Add spacing to LinkedListExtensions

--- MBBSEmu/Extensions/LinkedListExtensions.cs
+++ MBBSEmu/Extensions/LinkedListExtensions.cs
@@ -2,10 +2,13 @@ using System.Collections.Generic;
 
 public static class LinkedListExtensions
 {
+    /// <summary>
+    ///     Allows enumeration of LinkedListNode over a LinkedList.
+    /// </summary>
     public static IEnumerable<LinkedListNode<T>> EnumerateNodes<T>(this LinkedList<T> list)
     {
         var node = list.First;
-        while(node != null)
+        while (node != null)
         {
             yield return node;
             node = node.Next;
