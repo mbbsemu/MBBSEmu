cf146b383d46fa2528e9a45a9572ef4943777991
paladine <todd@voidpointer.org>
2021-03-21 17:12:12 -0600

    Add strlen assembly unit test (#446)
    * Add strlen assembly test
    
    * Complete strlen assembly test

--- MBBSEmu.Tests/CPU/SCASB_Tests.cs
+++ MBBSEmu.Tests/CPU/SCASB_Tests.cs
@@ -1,7 +1,11 @@
-﻿using Iced.Intel;
+﻿using FluentAssertions;
+using Iced.Intel;
 using MBBSEmu.CPU;
 using MBBSEmu.Extensions;
+using MBBSEmu.Memory;
+using System.Text;
 using Xunit;
+using static Iced.Intel.AssemblerRegisters;
 
 namespace MBBSEmu.Tests.CPU
 {
@@ -181,6 +185,61 @@ namespace MBBSEmu.Tests.CPU
             Assert.Equal(0x4, mbbsEmuCpuRegisters.CX);
             Assert.Equal(0xFFFF, mbbsEmuCpuRegisters.DI); //DI is decremented after the comparison, which would overflow from 0x0->0xFFFF
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.ZF));
+        }
+
+        [Theory]
+        [InlineData("test", 4)]
+        [InlineData("", 0)]
+        [InlineData("This is super long", 18)]
+        [InlineData("This is\0 super long", 7)]
+        public void strlen(string str, ushort expectedLength)
+        {
+            Reset();
+            mbbsEmuProtectedModeMemoryCore.AddSegment(0);
+            mbbsEmuProtectedModeMemoryCore.AddSegment(2);
+            mbbsEmuCpuRegisters.DS = mbbsEmuCpuRegisters.ES = 2;
+            mbbsEmuCpuRegisters.SS = 0;
+            mbbsEmuCpuRegisters.SP = 0x100;
+
+            var strPtr = new FarPtr(mbbsEmuCpuRegisters.DS, 0x1000);
+
+            mbbsEmuMemoryCore.SetArray(strPtr, Encoding.ASCII.GetBytes(str + "\0"));
+
+            var instructions = new Assembler(16);
+            var strlen = instructions.CreateLabel();
+
+            instructions.push(strPtr.Offset);
+            instructions.call(strlen);
+            instructions.hlt();
+
+            /*
+            str             = word ptr  4
+            */
+            instructions.Label(ref strlen);
+            instructions.push(bp);
+            instructions.mov(bp, sp);
+
+            instructions.push(di);
+            instructions.push(cx);
+            instructions.mov(di, __word_ptr[bp + 4]); // str
+            instructions.mov(cx, -1);
+            instructions.xor(al, al); // search for null
+            instructions.cld();
+            instructions.repne.scasb();
+            instructions.not(cx);
+            instructions.mov(ax, cx);
+            instructions.dec(ax);
+
+            instructions.pop(cx);
+            instructions.pop(di);
+            instructions.pop(bp);
+            instructions.ret();
+            CreateCodeSegment(instructions);
+
+            while (!mbbsEmuCpuRegisters.Halt)
+                mbbsEmuCpuCore.Tick();
+
+            mbbsEmuCpuRegisters.AX.Should().Be(expectedLength);
         }
     }
 }
