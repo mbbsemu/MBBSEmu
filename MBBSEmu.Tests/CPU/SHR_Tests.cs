e0865dca13f851792c88ae5fe53c5df7e544d3af
Todd Jeffreys <todd@voidpointer.org>
2021-03-21 14:31:40 -0600

    Fix SHR tests

--- MBBSEmu.Tests/CPU/SHR_Tests.cs
+++ MBBSEmu.Tests/CPU/SHR_Tests.cs
@@ -9,8 +9,10 @@ namespace MBBSEmu.Tests.CPU
     public class SHR_Tests : CpuTestBase
     {
         [Theory]
-        [InlineData(0xFFFF, 0x7FFF)]
-        public void SHR_AX_CarryFlag_SignFlag(ushort axValue, ushort axExpectedValue)
+        [InlineData(0xFFFF, 0x7FFF, true)]
+        [InlineData(0x7FFF, 0x3FFF, false)]
+        [InlineData(0x3FFF, 0x1FFF, false)]
+        public void SHR_AX_CarryFlag_SignFlag(ushort axValue, ushort axExpectedValue, bool overflow)
         {
             Reset();
 
@@ -29,12 +31,12 @@ namespace MBBSEmu.Tests.CPU
             //Verify Flags
             Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.CF));
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.ZF));
-            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
+            Assert.Equal(overflow, mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
         }
 
         [Theory]
-        [InlineData(0x7FFF, 0xFFFE)]
+        [InlineData(0x7FFF, 0x3FFF)]
         public void SHR_AX_OverflowFlag_SignFlag(ushort axValue, ushort axExpectedValue)
         {
             Reset();
@@ -42,7 +44,7 @@ namespace MBBSEmu.Tests.CPU
             mbbsEmuCpuRegisters.AX = axValue;
 
             var instructions = new Assembler(16);
-            instructions.shl(ax, 1);
+            instructions.shr(ax, 1);
             CreateCodeSegment(instructions);
 
             //Process Instruction
@@ -52,10 +54,10 @@ namespace MBBSEmu.Tests.CPU
             Assert.Equal(axExpectedValue, mbbsEmuCpuRegisters.AX);
 
             //Verify Flags
-            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.CF));
+            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.CF));
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.ZF));
-            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
-            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
         }
     }
 }
