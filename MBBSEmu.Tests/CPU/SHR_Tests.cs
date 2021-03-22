4c444b201c20972b4cfcf197b5f68c69f1c0d1c0
Todd Jeffreys <todd@voidpointer.org>
2021-03-21 19:13:47 -0600

    Add clamping to large shifts (max 31).
    Add unit tests

--- MBBSEmu.Tests/CPU/SHR_Tests.cs
+++ MBBSEmu.Tests/CPU/SHR_Tests.cs
@@ -59,5 +59,53 @@ namespace MBBSEmu.Tests.CPU
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
             Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
         }
+
+        [Fact]
+        public void SHR_AX_Big_Shift()
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.AX = 0xFFFF;
+
+            var instructions = new Assembler(16);
+            instructions.shr(ax, 35); // will clamp to 3
+            CreateCodeSegment(instructions);
+
+            //Process Instruction
+            mbbsEmuCpuCore.Tick();
+
+            //Verify Results
+            Assert.Equal(0x1FFF, mbbsEmuCpuRegisters.AX);
+
+            //Verify Flags
+            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.CF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.ZF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
+        }
+
+        [Fact]
+        public void SHR_AH_Big_Shift()
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.AH = 0xFF;
+
+            var instructions = new Assembler(16);
+            instructions.shr(ah, 35); // will clamp to 3
+            CreateCodeSegment(instructions);
+
+            //Process Instruction
+            mbbsEmuCpuCore.Tick();
+
+            //Verify Results
+            Assert.Equal(0x1F, mbbsEmuCpuRegisters.AH);
+
+            //Verify Flags
+            Assert.True(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.CF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.ZF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.OF));
+            Assert.False(mbbsEmuCpuRegisters.F.IsFlagSet((ushort)EnumFlags.SF));
+        }
     }
 }
