bea2771e42d84d9d7669f132163ae24d053233c5
Eric Nusbaum <eric@enusbaum.com>
2021-03-26 06:42:05 -0400

    Added INT 10h Comments

--- MBBSEmu/DOS/Interrupts/Int10h.cs
+++ MBBSEmu/DOS/Interrupts/Int10h.cs
@@ -30,13 +30,19 @@ namespace MBBSEmu.DOS.Interrupts
                     AlternateFunctionSelect_0x12();
                     return;
                 case 0x08:
-                    ReadAttributes_CharacterAtCursorPosition();
+                    ReadAttributes_CharacterAtCursorPosition_0x08();
                     return;
                     
             }
         }
 
-
+        /// <summary>
+        /// INT 10 - AH = 0Fh VIDEO - GET CURRENT VIDEO MODE
+        /// Return: AH = number of columns on screen
+        /// AL = current video mode (see INT 10h/AH=00h)
+        /// BH = current active display page
+        /// Note: if mode was set with bit 7 set ("no blanking"), the returned mode will also have bit 7 set
+        /// </summary>
         private void GetCurrentVideoMode_0x0F()
         {
             _registers.AH = 80; //Columns
@@ -44,13 +50,27 @@ namespace MBBSEmu.DOS.Interrupts
             _registers.BH = 0x00; //Initial Page?
         }
 
+        /// <summary>
+        /// INT 10 - AH = 12h VIDEO - ALTERNATE FUNCTION SELECT (PS, EGA, VGA, MCGA
+        /// </summary>
         private void AlternateFunctionSelect_0x12()
         {
             switch (_registers.BL)
             {
                 case 0x10: //Return EGA Information
                 {
-                    _registers.BH = 0; //Color Mode
+                        /*
+                         * BL = 10h: return EGA information
+                           Return: BH = 0: color mode in effect (3Dx)
+                           1: mono mode in effect (3Bx)
+                           BL = 0: 64k bytes memory installed
+                           1: 128k bytes memory installed
+                           2: 192k bytes memory installed
+                           3: 256k bytes memory installed
+                           CH = feature bits
+                           CL = switch settings
+                         */
+                        _registers.BH = 0; //Color Mode
                     _registers.BL = 3; //256k bytes Memory Installed
                     _registers.CH = 0; //TODO -- Feature Bits?
                     _registers.CL = 0; //TODO -- Switch Settings?
@@ -59,7 +79,13 @@ namespace MBBSEmu.DOS.Interrupts
             }
         }
 
-        private void ReadAttributes_CharacterAtCursorPosition()
+        /// <summary>
+        /// INT 10 - AH = 08h VIDEO - READ ATTRIBUTES/CHARACTER AT CURSOR POSITION
+        /// BH = display page
+        /// Return: AL = character
+        /// AH = attribute of character (alpha modes)
+        /// </summary>
+        private void ReadAttributes_CharacterAtCursorPosition_0x08()
         {
             // Input: _registers.BL == Display Page
 
