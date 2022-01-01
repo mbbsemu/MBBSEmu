41d9d88e442136e135da4ede4d8783360b0edc94
Jonathan Sider <jsider@gmail.com>
2022-01-01 08:19:09 -0600

    Add FMULP unit test

---
+++ MBBSEmu.Tests/CPU/FMULP_tests.cs
@@ -0,0 +1,33 @@
+﻿using Iced.Intel;
+using Xunit;
+using static Iced.Intel.AssemblerRegisters;
+
+namespace MBBSEmu.Tests.CPU
+{
+    public class FMULP_Tests : CpuTestBase
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(1, .5)]
+        [InlineData(0, 0)]
+        [InlineData(6, 0)]
+        public void FMULP_Test(float ST0Value, float ST1Value)
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.Fpu.SetStackTop(1);
+            mbbsEmuCpuCore.FpuStack[1] = ST0Value; //ST0
+            mbbsEmuCpuCore.FpuStack[0] = ST1Value; //ST1
+
+            var instructions = new Assembler(16);
+            instructions.fmulp(st1, st0);
+            CreateCodeSegment(instructions);
+
+            mbbsEmuCpuCore.Tick();
+
+            var expectedValue = ST1Value * ST0Value;
+
+            Assert.Equal(expectedValue, mbbsEmuCpuCore.FpuStack[mbbsEmuCpuRegisters.Fpu.GetStackTop()]);
+        }
+    }
+}
