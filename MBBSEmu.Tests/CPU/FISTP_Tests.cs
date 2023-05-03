bdc2a1a31401e0961380c4a5bc3504fe8195b794
Eric P. Nusbaum <eric@enusbaum.com>
2023-05-03 08:36:25 -0400

    FISTP Fixes + CPU Exception Handling
    - Wrapped `tick()` in Try/Catch to dump CPU state in the event of an exception to help with debugging
    - Added support for both SS & DS as destination segments for `FISTP`
    - Additional `FISTP` Unit Tests

--- MBBSEmu.Tests/CPU/FISTP_Tests.cs
+++ MBBSEmu.Tests/CPU/FISTP_Tests.cs
@@ -17,7 +17,7 @@ namespace MBBSEmu.Tests.CPU
         [InlineData((double)31337, 31337, 0)]
         [InlineData(-0.0d, 0, 0)]
         [InlineData(double.NaN, 0, 1)]
-        public void FISTP_Test_M16(double ST0Value, short expectedvalue, ushort expectedControlWord)
+        public void FISTP_Test_M16_DS(double ST0Value, short expectedvalue, ushort expectedControlWord)
         {
             Reset();
 
@@ -44,7 +44,7 @@ namespace MBBSEmu.Tests.CPU
         [InlineData((double)(int.MinValue), int.MinValue, 0)]
         [InlineData(-0.0d, 0, 0)]
         [InlineData(double.NaN, 0, 1)]
-        public void FISTP_Test_M32(double ST0Value, int expectedvalue, ushort expectedControlWord)
+        public void FISTP_Test_M32_DS(double ST0Value, int expectedvalue, ushort expectedControlWord)
         {
             Reset();
 
@@ -70,7 +70,7 @@ namespace MBBSEmu.Tests.CPU
         [InlineData((double)(long.MinValue), long.MinValue, 0)]
         [InlineData(-0.0d, 0, 0)]
         [InlineData(double.NaN, 0, 1)]
-        public void FISTP_Test_M64(double ST0Value, long expectedvalue, ushort expectedControlWord)
+        public void FISTP_Test_M64_DS(double ST0Value, long expectedvalue, ushort expectedControlWord)
         {
             Reset();
 
@@ -87,6 +87,88 @@ namespace MBBSEmu.Tests.CPU
             mbbsEmuCpuCore.Tick();
 
             Assert.Equal(expectedvalue, BitConverter.ToInt64(mbbsEmuMemoryCore.GetArray(2, 0, 8)));
+            Assert.Equal(0, mbbsEmuCpuRegisters.Fpu.GetStackTop());
+            Assert.Equal(expectedControlWord, mbbsEmuCpuRegisters.Fpu.ControlWord & FPU_CONTROLWORD_EXCEPTION_MASK);
+        }
+
+        [Theory]
+        [InlineData((double)(short.MaxValue), short.MaxValue, 0)]
+        [InlineData((double)(short.MaxValue + 1), 0, 1)]
+        [InlineData((double)(short.MinValue), short.MinValue, 0)]
+        [InlineData((double)(short.MinValue - 1), 0, 1)]
+        [InlineData((double)31337, 31337, 0)]
+        [InlineData(-0.0d, 0, 0)]
+        [InlineData(double.NaN, 0, 1)]
+        public void FISTP_Test_M16_SS(double ST0Value, short expectedvalue, ushort expectedControlWord)
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.SS = 0;
+            mbbsEmuProtectedModeMemoryCore.AddSegment(0);
+            mbbsEmuCpuRegisters.Fpu.SetStackTop(1);
+            mbbsEmuCpuRegisters.Fpu.ControlWord = 0;
+            mbbsEmuCpuCore.FpuStack[1] = ST0Value;
+
+            var instructions = new Assembler(16);
+            instructions.fistp(__word_ptr.ss[0]);
+            CreateCodeSegment(instructions);
+
+            mbbsEmuCpuCore.Tick();
+
+            Assert.Equal(expectedvalue, (short)mbbsEmuMemoryCore.GetWord(mbbsEmuCpuRegisters.SS, 0));
+            Assert.Equal(0, mbbsEmuCpuRegisters.Fpu.GetStackTop());
+            Assert.Equal(expectedControlWord, mbbsEmuCpuRegisters.Fpu.ControlWord & FPU_CONTROLWORD_EXCEPTION_MASK);
+        }
+
+        [Theory]
+        [InlineData((double)(int.MaxValue), int.MaxValue, 0)]
+        [InlineData(31337.1d, 31337, 0)]
+        [InlineData((double)(int.MinValue), int.MinValue, 0)]
+        [InlineData(-0.0d, 0, 0)]
+        [InlineData(double.NaN, 0, 1)]
+        public void FISTP_Test_M32_SS(double ST0Value, int expectedvalue, ushort expectedControlWord)
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.SS = 0;
+            mbbsEmuProtectedModeMemoryCore.AddSegment(0);
+            mbbsEmuCpuRegisters.Fpu.SetStackTop(1);
+            mbbsEmuCpuRegisters.Fpu.ControlWord = 0;
+            mbbsEmuCpuCore.FpuStack[1] = ST0Value;
+
+            var instructions = new Assembler(16);
+            instructions.fistp(__dword_ptr.ss[0]);
+            CreateCodeSegment(instructions);
+
+            mbbsEmuCpuCore.Tick();
+
+            Assert.Equal(expectedvalue, BitConverter.ToInt32(mbbsEmuMemoryCore.GetArray(mbbsEmuCpuRegisters.SS, 0, 4)));
+            Assert.Equal(0, mbbsEmuCpuRegisters.Fpu.GetStackTop());
+            Assert.Equal(expectedControlWord, mbbsEmuCpuRegisters.Fpu.ControlWord & FPU_CONTROLWORD_EXCEPTION_MASK);
+        }
+
+        [Theory]
+        //[InlineData((double)(long.MaxValue), long.MaxValue, 0)] //Overflows -- need to figure out why
+        [InlineData((double)(long.MinValue), long.MinValue, 0)]
+        [InlineData(-0.0d, 0, 0)]
+        [InlineData(double.NaN, 0, 1)]
+        public void FISTP_Test_M64_SS(double ST0Value, long expectedvalue, ushort expectedControlWord)
+        {
+            Reset();
+
+            mbbsEmuCpuRegisters.SS = 0;
+            mbbsEmuProtectedModeMemoryCore.AddSegment(0);
+            mbbsEmuCpuRegisters.Fpu.SetStackTop(1);
+            mbbsEmuCpuRegisters.Fpu.ControlWord = 0;
+            mbbsEmuCpuCore.FpuStack[1] = ST0Value;
+
+            var instructions = new Assembler(16);
+            instructions.fistp(__qword_ptr.ss[0]);
+            CreateCodeSegment(instructions);
+
+            mbbsEmuCpuCore.Tick();
+
+            Assert.Equal(expectedvalue, BitConverter.ToInt64(mbbsEmuMemoryCore.GetArray(mbbsEmuCpuRegisters.SS, 0, 8)));
             Assert.Equal(0, mbbsEmuCpuRegisters.Fpu.GetStackTop());
             Assert.Equal(expectedControlWord, mbbsEmuCpuRegisters.Fpu.ControlWord & FPU_CONTROLWORD_EXCEPTION_MASK);
         }
