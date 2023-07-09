3669747760e5cac1e67acdc4d7993bdd9fd50b92
Eric P. Nusbaum <eric@enusbaum.com>
2023-07-09 16:19:22 -0400

    Enhanced Unit Tests
    - Enhanced Unit Tests to ensure proper new SEED value is saved
    - Removed older tests for `rand()`

--- MBBSEmu.Tests/ExportedModules/Majorbbs/rand_Tests.cs
+++ MBBSEmu.Tests/ExportedModules/Majorbbs/rand_Tests.cs
@@ -11,17 +11,17 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
         private const int RAND_ORDINAL = 486;
 
         [Theory]
-        [InlineData(1234, 1356)]
-        [InlineData(2236414843, 29133)]
-        [InlineData(4056779384, 21998)]
-        [InlineData(3589159641, 27018)]
-        [InlineData(1770671342, 30398)]
-        [InlineData(4139675975, 13799)]
-        [InlineData(904336820, 26300)]
-        [InlineData(3871102533, 16520)]
-        [InlineData(3230196298, 26957)]
-        [InlineData(1766679891, 21523)]
-        public void srandTest(uint seed, ushort expectedRandom)
+        [InlineData(1234, 1356, 2236414843)]
+        [InlineData(2236414843, 29133, 4056779384)]
+        [InlineData(4056779384, 21998, 3589159641)]
+        [InlineData(3589159641, 27018, 1770671342)]
+        [InlineData(1770671342, 30398, 4139675975)]
+        [InlineData(4139675975, 13799, 904336820)]
+        [InlineData(904336820, 26300, 3871102533)]
+        [InlineData(3871102533, 16520, 3230196298)]
+        [InlineData(3230196298, 26957, 1766679891)]
+        [InlineData(1766679891, 21523, 1410548784)]
+        public void srandTest(uint seed, ushort expectedRandom, uint newSeed)
         {
             Reset();
 
@@ -31,6 +31,7 @@ namespace MBBSEmu.Tests.ExportedModules.Majorbbs
 
             //Verify Results
             Assert.Equal(expectedRandom, mbbsEmuCpuRegisters.AX);
+            Assert.Equal(newSeed, mbbsEmuMemoryCore.GetDWord("RANDSEED"));
         }
     }
 }
