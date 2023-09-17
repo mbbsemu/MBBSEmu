693d0980e03e28bd56ae5ab742c14b7c7c7a0537
Todd Jeffreys <todd@voidpointer.org>
2023-09-17 16:42:28 -0600

    Get things mostly working. wbtrv32_test executes on my machine at least

--- wbtrv32/netsqlite/SQLiteInitializer.cs
+++ wbtrv32/netsqlite/SQLiteInitializer.cs
@@ -4,7 +4,6 @@
     {
         public static void Initialize()
         {
-            //SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
             SQLitePCL.Batteries.Init();
         }
     }
