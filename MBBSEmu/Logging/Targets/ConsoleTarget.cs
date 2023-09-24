3e10cde72baa0c08bbec94db095e96301c5f3b07
Eric P. Nusbaum <eric@enusbaum.com>
2023-09-24 13:02:26 -0400

    Add TODO for Reflection in Logger Target

--- MBBSEmu/Logging/Targets/ConsoleTarget.cs
+++ MBBSEmu/Logging/Targets/ConsoleTarget.cs
@@ -18,6 +18,7 @@ namespace MBBSEmu.Logging.Targets
             var logLevel = ((EnumLogLevel)logEntry[1]).ToString();
 
             //Use reflection to get the name of the class calling this method
+            //TODO: We should move this reflection up the stack to the log factory in GetLogger<T>() and pass it into the logger so we don't have to do this every time
             var callingType = new System.Diagnostics.StackTrace().GetFrame(3)?.GetMethod()?.DeclaringType;
 
             Console.WriteLine($"[{DateTime.Now:O}] {callingType} [{logLevel}] {logMessage}");
