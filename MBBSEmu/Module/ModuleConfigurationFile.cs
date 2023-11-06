a932a9897627afbc9713340b5ddaf6eb438a1400
Eric P. Nusbaum <eric@enusbaum.com>
2023-11-05 19:31:01 -0500

    Add "BasePath" option to Module Configuration JSON
    - Add `BasePath` Option to Module Configuration JSON
    - Create new JSON ModuleConfiguration Converter that loads Modules from the JSON and applies Base Path to each deserialized Module
    - ModeConfiguration will now apply `BasePath` to a modules specified path if it is set, and the Module path specified isnt a  Root path

--- MBBSEmu/Module/ModuleConfigurationFile.cs
+++ MBBSEmu/Module/ModuleConfigurationFile.cs
@@ -2,8 +2,19 @@
 
 namespace MBBSEmu.Module
 {
+    /// <summary>
+    ///     Class Representing a Deserialized Module Configuration File (JSON)
+    /// </summary>
     public class ModuleConfigurationFile
     {
+        /// <summary>
+        ///     Base Path for all Modules defined within the Module Configuration File
+        /// </summary>
+        public string BasePath { get; set; }
+
+        /// <summary>
+        ///     Array of Modules to be loaded by MBBSEmu
+        /// </summary>
         public List<ModuleConfiguration> Modules { get; set; }
     }
 }
