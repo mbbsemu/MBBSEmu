56339ecd6f185eafbefe90d641400d22d786dcfd
Eric P. Nusbaum <eric@enusbaum.com>
2024-08-26 21:38:19 -0400

    Add Control Word to Unit Test

--- MBBSEmu.Tests/CPU/FRNDINT_Tests.cs
+++ MBBSEmu.Tests/CPU/FRNDINT_Tests.cs
@@ -10,12 +10,15 @@ namespace MBBSEmu.Tests.CPU
         [InlineData(0.1d, 0d)]
         [InlineData(1.9d, 2d)]
         [InlineData(-1.9d, -2d)]
-        [InlineData(0.5d, 1d)]
+        [InlineData(0.5d, 0d)]
         [InlineData(0.49999999d, 0d)]
         public void FRNDINT_Test(double ST0Value, double expectedValue)
         {
             Reset();
             mbbsEmuCpuCore.FpuStack[mbbsEmuCpuRegisters.Fpu.GetStackTop()] = ST0Value;
+
+            //Set FPU Control Word to set rounding to even
+            mbbsEmuCpuRegisters.Fpu.ControlWord = 0x0000000C;
 
             var instructions = new Assembler(16);
             instructions.frndint();
