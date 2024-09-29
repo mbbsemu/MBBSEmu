83ed52b21d2b5afdf11e985863bed0298937b679
Eric P. Nusbaum <eric@enusbaum.com>
2024-09-29 13:48:21 -0400

    Implement new Struct Layout
    - Structs now implement a new Abstract Base Class that handles the memory access for values
    - Base Class can be used on any memory resident struct
    - Supports Structs in Channel arrays and single instance structs

---
+++ MBBSEmu/HostProcess/Structs/MemoryResidentStructBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using MBBSEmu.Memory;
+
+namespace MBBSEmu.HostProcess.Structs
+{
+    /// <summary>
+    ///     Abstract Base Class for Memory Resident Structs that are stored in the MemoryCore
+    ///
+    ///     This class is used to provide a common interface for accessing memory resident structs
+    /// </summary>
+    /// <param name="structName"></param>
+    /// <param name="size"></param>
+    public abstract class MemoryResidentStructBase(string structName, ushort size)
+    {
+        /// <summary>
+        ///     Gets a reference to the system memory
+        /// </summary>
+        private readonly IMemoryCore MemoryCore = ProtectedModeMemoryCore.GetInstance(null);
+
+        /// <summary>
+        ///     Name of the Struct to be used in the MemoryCore
+        /// </summary>
+        private string StructName { get; } = structName;
+
+        /// <summary>
+        ///     Channel Number for this User
+        /// 
+        ///     Used to calculate the offset of the users data in memory. We set -1 to denote that it hasn't been set yet
+        ///
+        ///     If there is only one copy of this struct in memory, ChannelNumber should be set to 0
+        /// </summary>
+        public short ChannelNumber { get; set; } = -1;
+
+        /// <summary>
+        ///     Size of the Struct in Bytes
+        /// </summary>
+        private ushort Size { get; } = size;
+
+        /// <summary>
+        ///     Essentially a pointer to the data in memory for the specified struct
+        /// </summary>
+        public Span<byte> Data
+        {
+            get
+            {
+                if (ChannelNumber == -1)
+                    throw new InvalidDataException("Channel Number must be set before accessing Data");
+
+                //Get the pointer to the specified Struct by Variable Name of the same name as the Struct
+                var userPointer = MemoryCore.GetVariablePointer(StructName);
+
+                //Calculate any channel offset if this struct exists for each channel
+                userPointer.Offset += (ushort)(Size * ChannelNumber);
+
+                return MemoryCore.GetArray(userPointer, Size);
+            }
+            set => value.CopyTo(Data);
+        }
+    }
+}
--- MBBSEmu/HostProcess/Structs/User.cs
+++ MBBSEmu/HostProcess/Structs/User.cs
@@ -1,108 +1,110 @@
 ﻿using System;
-using System.IO;
+using System.Drawing;
 using MBBSEmu.Memory;
 
 namespace MBBSEmu.HostProcess.Structs
 {
     /// <summary>
     ///     USER Struct as defined in MAJORBBS.H
+    ///
+    ///     41 Bytes in Total Length
     /// </summary>
-    public class User
+    public class User : MemoryResidentStructBase
     {
         public short UserClass
         {
-            get => BitConverter.ToInt16(Data, 0);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 0, sizeof(short));
+            get => BitConverter.ToInt16(Data);
+            set => BitConverter.GetBytes(value).CopyTo(Data[..sizeof(short)]);
         }
 
         public FarPtr Keys
         {
-            get => new FarPtr(new ReadOnlySpan<byte>(Data).Slice(2,4));
-            set => Array.Copy(value.Data, 0, Data, 2, FarPtr.Size);
+            get => new FarPtr(Data.Slice(2, 4));
+            set => value.Data.CopyTo(Data.Slice(2, FarPtr.Size));
         }
 
         public short State
         {
-            get => BitConverter.ToInt16(Data, 6);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 6, sizeof(short));
+            get => BitConverter.ToInt16(Data[6..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(6, sizeof(short)));
         }
 
         public short Substt
         {
-            get => BitConverter.ToInt16(Data, 8);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 8, sizeof(short));
+            get => BitConverter.ToInt16(Data[8..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(8, sizeof(short)));
         }
 
         public short Lofstt
         {
-            get => BitConverter.ToInt16(Data, 10);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 10, sizeof(short));
+            get => BitConverter.ToInt16(Data[10..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(10, 2));
         }
 
         public short Usetmr
         {
-            get => BitConverter.ToInt16(Data, 12);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 12, sizeof(short));
+            get => BitConverter.ToInt16(Data[12..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(12, sizeof(short)));
         }
 
         public short Minut4
         {
-            get => BitConverter.ToInt16(Data, 14);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 14, sizeof(short));
+            get => BitConverter.ToInt16(Data[14..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(14, sizeof(short)));
         }
 
         public short Countr
         {
-            get => BitConverter.ToInt16(Data, 16);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 16, sizeof(short));
+            get => BitConverter.ToInt16(Data[16..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(16, sizeof(short)));
         }
 
         public short Pfnacc
         {
-            get => BitConverter.ToInt16(Data, 18);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 18, sizeof(short));
+            get => BitConverter.ToInt16(Data[18..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(18, sizeof(short)));
         }
 
         public uint Flags
         {
-            get => BitConverter.ToUInt32(Data, 20);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 20, sizeof(uint));
+            get => BitConverter.ToUInt32(Data[20..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(20, sizeof(uint)));
         }
 
         public ushort Baud
         {
-            get => BitConverter.ToUInt16(Data, 24);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 24, sizeof(short));
+            get => BitConverter.ToUInt16(Data[24..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(24, sizeof(ushort)));
         }
 
         public short Crdrat
         {
-            get => BitConverter.ToInt16(Data, 26);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 26, sizeof(short));
+            get => BitConverter.ToInt16(Data[26..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(26, sizeof(short)));
         }
 
         public short Nazapc
         {
-            get => BitConverter.ToInt16(Data, 28);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 28, sizeof(short));
+            get => BitConverter.ToInt16(Data[28..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(28, sizeof(short)));
         }
 
         public short Linlim
         {
-            get => BitConverter.ToInt16(Data, 30);
-            set => Array.Copy(BitConverter.GetBytes(value), 0, Data, 30, sizeof(short));
+            get => BitConverter.ToInt16(Data[30..]);
+            set => BitConverter.GetBytes(value).CopyTo(Data.Slice(30, sizeof(short)));
         }
 
         public FarPtr Clsptr
         {
-            get => new FarPtr(new ReadOnlySpan<byte>(Data).Slice(32, 4));
-            set => Array.Copy(value.Data, 0, Data, 32, FarPtr.Size);
+            get => new(Data.Slice(32, 4));
+            set => value.Data.CopyTo(Data.Slice(32, FarPtr.Size));
         }
 
         public FarPtr Polrou
         {
-            get => new FarPtr(new ReadOnlySpan<byte>(Data).Slice(36, 4));
-            set => Array.Copy(value.Data, 0, Data, 36, FarPtr.Size);
+            get => new FarPtr(Data.Slice(36, 4));
+            set => value.Data.CopyTo(Data.Slice(36, FarPtr.Size));
         }
 
         public byte lcstat
@@ -111,14 +113,13 @@ namespace MBBSEmu.HostProcess.Structs
             set => Data[40] = value;
         }
 
-        public byte[] Data;
+        /// <summary>
+        ///    Size of the User Struct in Bytes
+        /// </summary>
+        public static ushort Size => 41;
 
-        public const ushort Size = 41;
-
-        public User()
+        public User() : base(nameof(User), Size)
         {
-            Data = new byte[Size];
-
             UserClass = 6;
             Minut4 = 0xA00;
             Baud = 38400;
